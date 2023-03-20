b7f5d050a65f2f63dd7a8629a86b97356bedb666
Rick Moore <rick329mo@gmail.com>
2023-03-20 08:02:29 -0500

    continued styling navbar and sidebar

--- nextjs-portfolio/src/components/Navbar.tsx
+++ nextjs-portfolio/src/components/Navbar.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
-import { AiOutlineMenu} from 'react-icons/ai'
+import { AiOutlineClose, AiOutlineMenu} from 'react-icons/ai'
 
 const Navbar = () => {
   return (
     <div className='fixed w-full h-20 shadow-xl z-[100]'>
-        <div className= 'flex justify-between items-center w-full h-full px-2 2xl:px-16'>
+        <div className= 'flex justify-between items-center w-full h-full 2xl:px-16'>
             <Image src="/../public/assets/RM.png" alt="/" width='150' height='50'/>
-            <div>
+            <div  className='mr-6'>
                 <ul className='hidden md:flex'>
                     <Link href="/"><li className='ml-10 text-sm uppercase hover:border-b'>Home</li></Link>
                     <Link href="/"><li className='ml-10 text-sm uppercase hover:border-b'>About</li></Link>
@@ -17,8 +17,25 @@ const Navbar = () => {
                     <Link href="/"><li className='ml-10 text-sm uppercase hover:border-b'>Contact</li></Link>
                 </ul>
                 <div className='md:hidden'>
-                    <AiOutlineMenu />
+                    <AiOutlineMenu size={25}/>
                 </div>
+            </div>
+        </div>
+        <div className='fixed left-0 top-0 w-full h-screen bg-black/60'>
+            <div className='fixed left-0 top-0 px-10 h-screen w-[75%] sm:w-[60%] md:w-[45%] bg-[#ecf0f3] ease-in duration-500'>
+                <div>
+                    <div className='flex w-full items-center justify-between'>
+                        <Image src='/../public/assets/RM.png' alt='/' width='100' height='25'/>
+                        <div className='rounded-full shadow-lg shadow-gray-400 p-2 mb-3 cursor-pointer'>
+                            <AiOutlineClose />
+                        </div>
+                    </div>
+                    <div className='border-b border-gray-300 my-4'>
+                        <p className='text-sm lg:text-base'>Let's build something <span className='text-red-500'>legendary</span> together</p>
+                    </div>
+                    
+                </div>
+
             </div>
         </div>
     </div>
